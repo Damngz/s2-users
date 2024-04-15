7a837fb9391d6d5bc08322e6ce697bb350d61ab2
Dvvmn <da.munozgz@gmail.com>
2024-04-15 19:54:54 -0400

    chore: model

---
+++ src/main/java/com/s2/users/model/User.java
@@ -0,0 +1,63 @@
+package com.s2.users.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "password")
+  private String password;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "address")
+  private String address;
+
+  @Column(name = "role")
+  private String role;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getRole() {
+    return role;
+  }
+}
