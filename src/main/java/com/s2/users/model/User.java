54f84d75be05b6c37156edc9777da77b0b9bb240
damngz <da.munozgz@gmail.com>
2024-04-15 21:24:03 -0400

    chore: user modified blank name

--- src/main/java/com/s2/users/model/User.java
+++ src/main/java/com/s2/users/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="usuario")
@@ -15,6 +16,7 @@ public class User {
   @Column(name = "id")
   private Long id;
 
+  @NotBlank(message = "No puede ingresar un nombre de usuario vacío")
   @Column(name = "username")
   private String username;
 
@@ -37,27 +39,55 @@ public class User {
     return id;
   }
 
+  public void setId(Long id) {
+    this.id = id;
+  }
+
   public String getUsername() {
     return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
   }
 
   public String getPassword() {
     return password;
   }
 
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
   public String getEmail() {
     return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   public String getName() {
     return name;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public String getAddress() {
     return address;
   }
 
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
   public String getRole() {
     return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
   }
 }
