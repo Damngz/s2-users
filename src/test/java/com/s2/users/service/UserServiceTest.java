2026be2b8d6d256db50d3172bb707732125038a8
damngz <da.munozgz@gmail.com>
2024-05-01 00:56:54 -0400

    test: 4 tests for the exam

--- src/test/java/com/s2/users/service/UserServiceTest.java
+++ src/test/java/com/s2/users/service/UserServiceTest.java
@@ -1,8 +1,12 @@
 package com.s2.users.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +26,7 @@ public class UserServiceTest {
   private UserRepository userRepositoryMock;
 
   @Test
-  public void saveUserTest() {
+  public void saveUserServiceTest() {
     User user = new User();
     user.setUsername("UserTest");
     user.setPassword("12345678");
@@ -36,5 +40,25 @@ public class UserServiceTest {
     User result = userService.createUser(user);
 
     assertEquals("usertest@example.com", result.getEmail());
+  }
+
+  @Test
+  public void updateUserServiceTest() {
+    User existingUser = new User();
+    existingUser.setUsername("ExistingUser");
+    existingUser.setEmail("existinguser@example.com");
+
+    when(userRepositoryMock.existsById(0L)).thenReturn(true);
+    
+    User updatedUser = new User(); 
+    updatedUser.setUsername("UpdatedUser");
+    updatedUser.setEmail("updateduser@example.com");
+    
+    when(userRepositoryMock.save(updatedUser)).thenReturn(updatedUser);
+    
+    User result = userService.updateUser(0L, updatedUser);
+    
+    assertEquals("UpdatedUser", result.getUsername());
+    assertEquals("updateduser@example.com", result.getEmail());
   }
 }
